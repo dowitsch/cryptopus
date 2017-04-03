808d1f5499faa8c6ee4dcc96df20b34622a32d7b
Felix Ammann <ammann@puzzle.ch>
2017-04-03 20:29:44 +0200

    API Endpoint Groups Teams

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,6 +7,8 @@
 
 class ApiController < ApplicationController
 
+
+
   def render_json(data = nil)
     data = ActiveModelSerializers::SerializableResource.new(data).as_json
     render status: response_status, json: { data: data, messages: messages }
@@ -26,7 +28,59 @@ class ApiController < ApplicationController
     @team ||= ::Team.find(params[:team_id])
   end
 
+
+  def api_user
+    @api_user ||= User.find(params['api_id'])
+  end
+
+
   private
+
+  def authorize
+    if params[:api_key] || params[:api_id]
+      if !authenticated?
+        render status: :unauthorized, json: { messages: messages }
+        return
+      end
+    else
+      super
+    end
+  end
+
+  def authenticated?
+    api_id = params[:api_id]
+    api_key = params[:api_key]
+
+    api_user = User.find(api_id) if api_id.present?
+
+    if api_user
+      authenticate_api_user(api_id, api_key)
+    else
+      return false
+    end
+  end
+
+  def authenticate_api_user(api_id, api_key)
+    authenticator = Authentication::UserAuthenticator.new(params)
+
+    if authenticator.api_key_auth!
+      add_info 'Authenticated'
+      return true
+    else
+      add_error 'Wrong API-ID or API-Key'
+      return false
+    end
+  end
+
+  def refuse_if_not_teammember
+    team_id = params[:team_id]
+    return if team_id.nil?
+    team = Team.find(team_id)
+    return if team.teammember?(api_user.id)
+    add_error 'No Access to Team'
+    render_json
+    return
+  end
 
   def messages
     @messages ||=
@@ -40,4 +94,5 @@ class ApiController < ApplicationController
   def success_or_error
     messages[:errors].present? ? :internal_server_error : nil
   end
+
 end
